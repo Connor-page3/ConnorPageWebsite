2cf02e9d59a1e6218e54a1d46d7794049e39d60b
Connor Page <54785366+Connor-page3@users.noreply.github.com>
2026-07-15 21:18:42 -0400

    Improve portfolio layout and resume links (#2)
    * Center hero content
    
    * Remove duplicate resume page
    
    * Redesign experience timeline
    
    * Add official STARS program reference
    
    * Refine STARS experience presentation
    
    * Lighten experience section with frosted glass
    
    * Refine approach statement
    
    * Map approach statement to SDLC phases
    
    * Balance SDLC approach language
    
    * Finalize concise lifecycle approach

--- assets/js/resume.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-document.body.classList.remove("is-preload");
-
-const printButton = document.getElementById("print-resume");
-
-if (printButton) {
-	printButton.addEventListener("click", () => window.print());
-}
